46eee4d2378bec5c7cdae77265b6c65770b2d00f
sourav shinde <sourav shinde@LAPTOP-8K87II86>
2023-06-09 22:16:54 +0530

    a

---
+++ SeleniumAutomation1/src/All_Options.java
@@ -0,0 +1,16 @@
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class All_Options {
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.goodreads.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+
+}
