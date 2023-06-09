d7f8605fd069a13d0cd45814d72e7b359299d70e
sourav shinde <sourav shinde@LAPTOP-8K87II86>
2023-06-09 22:15:53 +0530

    s

---
+++ SeleniumAutomation1/src/NavigationAPI/Change_window.java
@@ -0,0 +1,16 @@
+package NavigationAPI;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Change_window {
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver = new ChromeDriver();
+		
+		
+
+	}
+
+}
